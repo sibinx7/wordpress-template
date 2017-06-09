8560f6118867085b1fb35271ff715f29ed1708e7
Sibin Xavier <sibinx7@gmail.com>
2017-06-09 19:37:09 +0530

    Change from home and gulp file

--- gulpfile.js
+++ gulpfile.js
@@ -3,23 +3,17 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
-
 var jquerySRC         = "./node_modules/jquery/dist/jquery.min.js";
 var tetherSRC         = "./node_modules/tether/dist/js/tether.min.js";
 var bootstrapSRC      = "./node_modules/bootstrap/dist/js/bootstrap.min.js";
 
-
 var mainStyleSCSS = "./source/sass/main.scss";
-
-var campanherJS = "./source/js/campanher.js";
 
 gulp.task('concat', function(){
   return gulp.src([jquerySRC,tetherSRC,bootstrapSRC])
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./js'))
 });
-
-
 gulp.task('sass', function(){
   return gulp.src([mainStyleSCSS])
     .pipe(sass({
@@ -28,10 +22,7 @@ gulp.task('sass', function(){
     .pipe(rename('campanher.css'))
     .pipe(gulp.dest('./css'))
 });
-
 gulp.task('watch', function(){
   gulp.watch('./source/sass/**/*.scss',['sass']);
 });
-
-
 gulp.task('default',['concat','sass']);
