29ccb1b3e7164f5b72d6f66cf569767eb7b7fc57
Sibin Xavier <sibinx7@gmail.com>
2017-06-09 19:32:59 +0530

    Common gulp tasks

---
+++ gulpfile.js
@@ -0,0 +1,37 @@
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var concat = require('gulp-concat');
+var rename = require('gulp-rename');
+
+
+var jquerySRC         = "./node_modules/jquery/dist/jquery.min.js";
+var tetherSRC         = "./node_modules/tether/dist/js/tether.min.js";
+var bootstrapSRC      = "./node_modules/bootstrap/dist/js/bootstrap.min.js";
+
+
+var mainStyleSCSS = "./source/sass/main.scss";
+
+var campanherJS = "./source/js/campanher.js";
+
+gulp.task('concat', function(){
+  return gulp.src([jquerySRC,tetherSRC,bootstrapSRC])
+    .pipe(concat('main.js'))
+    .pipe(gulp.dest('./js'))
+});
+
+
+gulp.task('sass', function(){
+  return gulp.src([mainStyleSCSS])
+    .pipe(sass({
+      outputStyle:'compressed'
+    }))
+    .pipe(rename('campanher.css'))
+    .pipe(gulp.dest('./css'))
+});
+
+gulp.task('watch', function(){
+  gulp.watch('./source/sass/**/*.scss',['sass']);
+});
+
+
+gulp.task('default',['concat','sass']);
