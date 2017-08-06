a44a67f27fac94170cf5338c533ade7dffb9ae8b
Sibin Xavier <sibinx7@gmail.com>
2017-08-06 12:14:21 +0530

    Update packages

--- gulpfile.js
+++ gulpfile.js
@@ -3,6 +3,16 @@ var sass = require('gulp-sass');
 var concat = require('gulp-concat');
 var rename = require('gulp-rename');
 
+var babel           = require("gulp-babel");
+var gulpBrowserify  = require("gulp-browserify");
+var browserify      = require("browserify");
+var babelify        = require("babelify");
+var source          = require("vinyl-source-stream")
+var buffer          = require('vinyl-buffer');
+var typeScript      = require('gulp-typescript');
+
+
+
 var jquerySRC         = "./node_modules/jquery/dist/jquery.min.js";
 var tetherSRC         = "./node_modules/tether/dist/js/tether.min.js";
 var bootstrapSRC      = "./node_modules/bootstrap/dist/js/bootstrap.min.js";
@@ -14,6 +24,19 @@ gulp.task('concat', function(){
     .pipe(concat('main.js'))
     .pipe(gulp.dest('./js'))
 });
+
+gulp.task('concatBabelScript', function(){
+	return browserify({
+		entries: './js-scss/js/main.js'
+	})
+	.transform("babelify",{
+		presets: ["es2015", "latest"]
+	})
+	.bundle()
+	.pipe(source("main-es6.js"))
+	.pipe(gulp.dest('./js'))
+});
+
 gulp.task('sass', function(){
   return gulp.src([mainStyleSCSS])
     .pipe(sass({
